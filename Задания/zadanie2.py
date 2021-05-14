b0652f09676f13be5969846e1ad6ba29dfd664f9
Adrenaline1 <zligostev.igor123@yandex.ru>
2021-05-14 17:22:59 +0300

    dasdasd

--- Задания/zadanie2.py
+++ Задания/zadanie2.py
@@ -3,6 +3,7 @@
 if __name__ == '__main__':
     a = str(input("Введите слово: "))
     s = a.split()
+    c = list(a)
     if len(s) >= 2:
         print("Введите одно слово")
         exit(1)
@@ -12,8 +13,10 @@ if __name__ == '__main__':
             print("Колличество букв в слове нечётное")
             exit(2)
         else:
+            print(c)
             r = ''
-            for i in range(0, len(a), 2):
-                r += a[i + 1] + a[i]
-            a = r
-        print("Результат работы программы: ", a)
+            for i in enumerate(c, 0):
+                j = i[0]
+                if i[0] % 2 == 0:
+                    r += a[j+1] + a[j]
+            print("Результат работы программы: ", r)
--- Задания/Задание 3.py
+++ Задания/Задание 3.py
@@ -3,9 +3,10 @@
 if __name__ == '__main__':
     a = str(input("Введите фразу: "))
     s = 0
-    for i in range(len(a)):
-        if a[i].isdigit():
-            s += int(a[i])
+    a = list(a)
+    for i in a:
+        if i.isdigit():
+            s += int(i)
     if s == 0:
         print("Чисел нет")
         exit(2)
